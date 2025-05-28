af315c10a3e6e71bbc31e3eaa130a1827a22771d
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-05-28 16:35:21 +0200

    End

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
-docker build -t maximeleblanc/multi-client:latest -t maximeleblanc/multi-client:$SHA -f ./client/Dockerfile client
-docker build -t maximeleblanc/multi-server:latest -t maximeleblanc/multi-server:$SHA -f ./server/Dockerfile server
-docker build -t maximeleblanc/multi-worker:latest -t maximeleblanc/multi-worker:$SHA -f ./worker/Dockerfile worker
-docker push maximeleblanc/multi-client:latest
-docker push maximeleblanc/multi-server:latest
-docker push maximeleblanc/multi-worker:latest
-docker push maximeleblanc/multi-client:$SHA
-docker push maximeleblanc/multi-server:$SHA
-docker push maximeleblanc/multi-worker:$SHA
+docker build -t maximeleblanc/multi-client-k8s:latest -t maximeleblanc/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t maximeleblanc/multi-server-k8s:latest -t maximeleblanc/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+docker build -t maximeleblanc/multi-worker-k8s:latest -t maximeleblanc/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+
+docker push maximeleblanc/multi-client-k8s:latest
+docker push maximeleblanc/multi-server-k8s:latest
+docker push maximeleblanc/multi-worker-k8s:latest
+
+docker push maximeleblanc/multi-client-k8s:$SHA
+docker push maximeleblanc/multi-server-k8s:$SHA
+docker push maximeleblanc/multi-worker-k8s:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=maximeleblanc/multi-server:$SHA
-kubectl set image deployments/client-deployment client=maximeleblanc/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=maximeleblanc/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=maximeleblanc/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=maximeleblanc/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=maximeleblanc/multi-worker-k8s:$SHA
